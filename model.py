ad0278a762ff1ffe98010e810c7d7269e543798b
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 08:40:00 +0700

    add main function and remove redundant code

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
+import os
+import neuralnet as nn
 from utils.common import exists
 import tensorflow as tf
-import neuralnet as nn
 import numpy as np
 
-# -----------------------------------------------------------
-#  FSRCNN
-# -----------------------------------------------------------
+class logger:
+    def __init__(self, path, values) -> None:
+        self.path = path
+        self.values = values
 
 class FSRCNN:
     def __init__(self, scale): 
@@ -21,14 +23,14 @@ class FSRCNN:
         self.ckpt = None
         self.ckpt_dir = None
         self.ckpt_man = None
-    
+
     def setup(self, optimizer, loss, metric, model_path):
         self.optimizer = optimizer
         self.loss = loss
         self.metric = metric
         # @the best model weights
         self.model_path = model_path
-    
+
     def load_checkpoint(self, ckpt_dir):
         self.ckpt_dir = ckpt_dir
         self.ckpt = tf.train.Checkpoint(step=tf.Variable(0), 
@@ -36,14 +38,14 @@ class FSRCNN:
                                         net=self.model)
         self.ckpt_man = tf.train.CheckpointManager(self.ckpt, ckpt_dir, max_to_keep=1)
         self.ckpt.restore(self.ckpt_man.latest_checkpoint)
-    
+
     def load_weights(self, filepath):
         self.model.load_weights(filepath)
 
     def predict(self, lr):
         sr = self.model(lr)
         return sr
-    
+
     def evaluate(self, dataset, batch_size=64):
         losses, metrics = [], []
         isEnd = False
@@ -57,9 +59,21 @@ class FSRCNN:
         loss = tf.reduce_mean(losses).numpy()
         return loss, metric
 
-    def train(self, train_set, valid_set, batch_size, 
-              steps, save_every=1, save_best_only=False):
-        
+    def train(self, train_set, valid_set, batch_size, steps, save_every=1,
+              save_best_only=False, save_log=False, log_dir=None):
+
+        if (save_log) and (log_dir is None):
+            ValueError("log_dir must be specified if save_log is True")
+        os.makedirs(log_dir, exist_ok=True)
+        dict_logger = {"loss":       logger(path=os.path.join(log_dir, "losses.npy"),      values=[]),
+                       "metric":     logger(path=os.path.join(log_dir, "metrics.npy"),     values=[]),
+                       "val_loss":   logger(path=os.path.join(log_dir, "val_losses.npy"),  values=[]),
+                       "val_metric": logger(path=os.path.join(log_dir, "val_metrics.npy"), values=[])}
+        for key in dict_logger.keys():
+            path = dict_logger[key].path
+            if exists(path):
+                dict_logger[key].values = np.load(path).tolist()
+
         cur_step = self.ckpt.step.numpy()
         max_steps = steps + self.ckpt.step.numpy()
 
@@ -69,34 +83,50 @@ class FSRCNN:
             prev_loss, _ = self.evaluate(valid_set)
             self.load_checkpoint(self.ckpt_dir)
 
-        loss_mean = tf.keras.metrics.Mean()
-        metric_mean = tf.keras.metrics.Mean()
+        loss_buffer = []
+        metric_buffer = []
         while cur_step < max_steps:
             cur_step += 1
             self.ckpt.step.assign_add(1)
             lr, hr, _ = train_set.get_batch(batch_size)
             loss, metric = self.train_step(lr, hr)
-            loss_mean(loss)
-            metric_mean(metric)
+            loss_buffer.append(loss)
+            metric_buffer.append(metric)
 
             if (cur_step % save_every == 0) or (cur_step >= max_steps):
+                loss = tf.reduce_mean(loss_buffer).numpy()
+                metric = tf.reduce_mean(metric_buffer).numpy()
                 val_loss, val_metric = self.evaluate(valid_set)
                 print(f"Step {cur_step}/{max_steps}",
-                      f"- loss: {loss_mean.result():.7f}",
-                      f"- {self.metric.__name__}: {metric_mean.result():.3f}",
+                      f"- loss: {loss:.7f}",
+                      f"- {self.metric.__name__}: {metric:.3f}",
                       f"- val_loss: {val_loss:.7f}",
                       f"- val_{self.metric.__name__}: {val_metric:.3f}")
-                loss_mean.reset_states()
-                metric_mean.reset_states()
-                self.ckpt_man.save(checkpoint_number=0)
                 
+                if save_log == True:
+                    dict_logger["loss"].values.append(loss)
+                    dict_logger["metric"].values.append(metric)
+                    dict_logger["val_loss"].values.append(val_loss)
+                    dict_logger["val_metric"].values.append(val_metric)
+                
+                loss_buffer = []
+                metric_buffer = []
+                self.ckpt_man.save(checkpoint_number=0)
+
                 if save_best_only and val_loss > prev_loss:
                     continue
                 prev_loss = val_loss
                 self.model.save_weights(self.model_path)
                 print(f"Save model to {self.model_path}\n")
+        
+        if save_log == True:
+            for key in dict_logger.keys():
+                logger_obj = dict_logger[key]
+                path = logger_obj.path
+                values = np.array(logger_obj.values, dtype=np.float32)
+                np.save(path, values)
 
-    @tf.function    
+    @tf.function
     def train_step(self, lr, hr):
         with tf.GradientTape() as tape:
             sr = self.model(lr, training=True)
@@ -105,3 +135,4 @@ class FSRCNN:
         gradient = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(gradient, self.model.trainable_variables))
         return loss, metric
+
