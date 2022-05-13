8387ec8d35ca068d96de0ee7094ebcc04690191c
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 08:40:06 +0700

    add main function and remove redundant code

--- train.py
+++ train.py
@@ -9,30 +9,39 @@ from model import FSRCNN
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--steps",            type=int,   default=100000,            help='-')
-parser.add_argument("--batch-size",       type=int,   default=128,               help='-')
-parser.add_argument("--scale",            type=int,   default=2,                 help='-')
-parser.add_argument("--save-every",       type=int,   default=100,               help='-')
-parser.add_argument("--save-best-only",   type=int,   default=0,                 help='-')
-parser.add_argument("--ckpt-dir",         type=str,   default="checkpoint/x2",   help='-')
+parser.add_argument("--steps",          type=int, default=100000, help='-')
+parser.add_argument("--scale",          type=int, default=2,      help='-')
+parser.add_argument("--batch-size",     type=int, default=128,    help='-')
+parser.add_argument("--save-every",     type=int, default=100,    help='-')
+parser.add_argument("--save-best-only", type=int, default=0,      help='-')
+parser.add_argument("--save-log",       type=int, default=0,      help='-')
+parser.add_argument("--ckpt-dir",       type=str, default="",     help='-')
 
+
+# -----------------------------------------------------------
+# global variables
+# -----------------------------------------------------------
 
 FLAG, unparsed = parser.parse_known_args()
 steps = FLAG.steps
 batch_size = FLAG.batch_size
-scale = FLAG.scale
-ckpt_dir = FLAG.ckpt_dir
 save_every = FLAG.save_every
-model_path = os.path.join(ckpt_dir, f"FSRCNN-x{scale}.h5")
+save_log = (FLAG.save_log == 1)
 save_best_only = (FLAG.save_best_only == 1)
+
+scale = FLAG.scale
+if scale not in [2, 3, 4]:
+    ValueError("scale must be 2, 3 or 4")
+
+ckpt_dir = FLAG.ckpt_dir
+if (ckpt_dir == "") or (ckpt_dir == "default"):
+    ckpt_dir = f"checkpoint/x{scale}"
+model_path = os.path.join(ckpt_dir, f"FSRCNN-x{scale}.h5")
 
 
 # -----------------------------------------------------------
 #  Init datasets
 # -----------------------------------------------------------
-
-if scale not in [1, 2, 3]:
-    ValueError("scale must be 2, 3 or 4")
 
 dataset_dir = "dataset"
 lr_crop_size = 10
@@ -51,15 +60,18 @@ valid_set.load_data()
 #  Train
 # -----------------------------------------------------------
 
-srcnn = FSRCNN(scale)
-srcnn.setup(optimizer=Adam(learning_rate=1e-3),
-            loss=MeanSquaredError(),
-            model_path=model_path,
-            metric=PSNR)
+def main():
+    model = FSRCNN(scale)
+    model.setup(optimizer=Adam(learning_rate=1e-3),
+                loss=MeanSquaredError(),
+                model_path=model_path,
+                metric=PSNR)
 
-srcnn.load_checkpoint(ckpt_dir)
-srcnn.train(train_set, valid_set, 
-            steps=steps, batch_size=batch_size,
-            save_best_only=save_best_only, 
-            save_every=save_every)
+    model.load_checkpoint(ckpt_dir)
+    model.train(train_set, valid_set, steps=steps, batch_size=batch_size,
+                save_best_only=save_best_only, save_every=save_every,
+                save_log=save_log, log_dir=ckpt_dir)
+
+if __name__ == "__main__":
+    main()
 
